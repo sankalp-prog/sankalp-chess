dc0dd959c8c7034260dc0f83c5bddb8f76af4c5a
sankalp-prog <planetsankalp@gmail.com>
2024-09-07 22:20:58 +0530

    Adding Components, chess board layout and some initial logic

--- src/App.vue
+++ src/App.vue
@@ -1,225 +1,286 @@
 <template>
-  <section>
-    <h1>Tic Tac Toe</h1>
-    <div id="grid">
-      <div class="square" v-for="square in squares" :key="square.id" @click="playerInput(square.id)">
-        {{ square.value }}
-      </div>
+  <div id="grid">
+    <div id="A8" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      A8
     </div>
-    <div v-if="result === 'player'">Player wins</div>
-    <div v-else-if="result === 'computer'">Computer wins</div>
-    <div v-else-if="result === 'draw'">It's a Draw</div>
-    <button @click="reset">Reset</button>
-    <select @change="setDifficulty">
-      <option value="random">Random</option>
-      <option value="medium">Medium</option>
-      <option value="impossible">Impossible</option>
-    </select>
-  </section>
+    <div id="B8" class="square even" @drop.prevent="drop" @dragover.prevent>
+      B8
+    </div>
+    <div id="C8" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      C8
+    </div>
+    <div id="D8" class="square even" @drop.prevent="drop" @dragover.prevent>
+      D8
+    </div>
+    <div id="E8" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      E8
+    </div>
+    <div id="F8" class="square even" @drop.prevent="drop" @dragover.prevent>
+      F8
+    </div>
+    <div id="G8" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      G8
+    </div>
+    <div id="H8" class="square even" @drop.prevent="drop" @dragover.prevent>
+      H8
+    </div>
+
+    <div id="A7" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      A7
+      <img
+        id="A7pawn"
+        src="https://static.vecteezy.com/system/resources/thumbnails/010/954/466/small/chess-pawn-icon-free-vector.jpg"
+        width="50"
+        height="50"
+        draggable="true"
+        @dragstart="drag"
+      />
+    </div>
+    <div id="B7" class="square even" @drop.prevent="drop" @dragover.prevent>
+      B7
+      <img
+        id="B7pawn"
+        src="https://static.vecteezy.com/system/resources/thumbnails/010/954/466/small/chess-pawn-icon-free-vector.jpg"
+        width="50"
+        height="50"
+        draggable="true"
+        @dragstart="drag"
+      />
+    </div>
+    <div id="C7" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      C7
+      <img
+        id="C7pawn"
+        src="https://static.vecteezy.com/system/resources/thumbnails/010/954/466/small/chess-pawn-icon-free-vector.jpg"
+        width="50"
+        height="50"
+        draggable="true"
+        @dragstart="drag"
+      />
+    </div>
+    <div id="D7" class="square even" @drop.prevent="drop" @dragover.prevent>
+      D7
+      <img
+        id="D7pawn"
+        src="https://static.vecteezy.com/system/resources/thumbnails/010/954/466/small/chess-pawn-icon-free-vector.jpg"
+        width="50"
+        height="50"
+        draggable="true"
+        @dragstart="drag"
+      />
+    </div>
+    <div id="E7" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      E7
+      <img
+        id="E7pawn"
+        src="https://static.vecteezy.com/system/resources/thumbnails/010/954/466/small/chess-pawn-icon-free-vector.jpg"
+        width="50"
+        height="50"
+        draggable="true"
+        @dragstart="drag"
+      />
+    </div>
+    <div id="F7" class="square even" @drop.prevent="drop" @dragover.prevent>
+      F7
+      <img
+        id="F7pawn"
+        src="https://static.vecteezy.com/system/resources/thumbnails/010/954/466/small/chess-pawn-icon-free-vector.jpg"
+        width="50"
+        height="50"
+        draggable="true"
+        @dragstart="drag"
+      />
+    </div>
+    <div id="G7" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      G7
+      <img
+        id="G7pawn"
+        src="https://static.vecteezy.com/system/resources/thumbnails/010/954/466/small/chess-pawn-icon-free-vector.jpg"
+        width="50"
+        height="50"
+        draggable="true"
+        @dragstart="drag"
+      />
+    </div>
+    <div id="H7" class="square even" @drop.prevent="drop" @dragover.prevent>
+      H7
+      <img
+        id="H7pawn"
+        src="https://static.vecteezy.com/system/resources/thumbnails/010/954/466/small/chess-pawn-icon-free-vector.jpg"
+        width="50"
+        height="50"
+        draggable="true"
+        @dragstart="drag"
+      />
+    </div>
+
+    <div id="A6" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      A6
+    </div>
+    <div id="B6" class="square even" @drop.prevent="drop" @dragover.prevent>
+      B6
+    </div>
+    <div id="C6" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      C6
+    </div>
+    <div id="D6" class="square even" @drop.prevent="drop" @dragover.prevent>
+      D6
+    </div>
+    <div id="E6" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      E6
+    </div>
+    <div id="F6" class="square even" @drop.prevent="drop" @dragover.prevent>
+      F6
+    </div>
+    <div id="G6" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      G6
+    </div>
+    <div id="H6" class="square even" @drop.prevent="drop" @dragover.prevent>
+      H6
+    </div>
+
+    <div id="A5" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      A5
+    </div>
+    <div id="B5" class="square even" @drop.prevent="drop" @dragover.prevent>
+      B5
+    </div>
+    <div id="C5" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      C5
+    </div>
+    <div id="D5" class="square even" @drop.prevent="drop" @dragover.prevent>
+      D5
+    </div>
+    <div id="E5" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      E5
+    </div>
+    <div id="F5" class="square even" @drop.prevent="drop" @dragover.prevent>
+      F5
+    </div>
+    <div id="G5" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      G5
+    </div>
+    <div id="H5" class="square even" @drop.prevent="drop" @dragover.prevent>
+      H5
+    </div>
+
+    <div id="A4" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      A4
+    </div>
+    <div id="B4" class="square even" @drop.prevent="drop" @dragover.prevent>
+      B4
+    </div>
+    <div id="C4" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      C4
+    </div>
+    <div id="D4" class="square even" @drop.prevent="drop" @dragover.prevent>
+      D4
+    </div>
+    <div id="E4" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      E4
+    </div>
+    <div id="F4" class="square even" @drop.prevent="drop" @dragover.prevent>
+      F4
+    </div>
+    <div id="G4" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      G4
+    </div>
+    <div id="H4" class="square even" @drop.prevent="drop" @dragover.prevent>
+      H4
+    </div>
+
+    <div id="A3" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      A3
+    </div>
+    <div id="B3" class="square even" @drop.prevent="drop" @dragover.prevent>
+      B3
+    </div>
+    <div id="C3" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      C3
+    </div>
+    <div id="D3" class="square even" @drop.prevent="drop" @dragover.prevent>
+      D3
+    </div>
+    <div id="E3" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      E3
+    </div>
+    <div id="F3" class="square even" @drop.prevent="drop" @dragover.prevent>
+      F3
+    </div>
+    <div id="G3" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      G3
+    </div>
+    <div id="H3" class="square even" @drop.prevent="drop" @dragover.prevent>
+      H3
+    </div>
+
+    <div id="A2" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      A2
+    </div>
+    <div id="B2" class="square even" @drop.prevent="drop" @dragover.prevent>
+      B2
+    </div>
+    <div id="C2" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      C2
+    </div>
+    <div id="D2" class="square even" @drop.prevent="drop" @dragover.prevent>
+      D2
+    </div>
+    <div id="E2" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      E2
+    </div>
+    <div id="F2" class="square even" @drop.prevent="drop" @dragover.prevent>
+      F2
+    </div>
+    <div id="G2" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      G2
+    </div>
+    <div id="H2" class="square even" @drop.prevent="drop" @dragover.prevent>
+      H2
+    </div>
+
+    <div id="A1" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      A1
+    </div>
+    <div id="B1" class="square even" @drop.prevent="drop" @dragover.prevent>
+      B1
+    </div>
+    <div id="C1" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      C1
+    </div>
+    <div id="D1" class="square even" @drop.prevent="drop" @dragover.prevent>
+      D1
+    </div>
+    <div id="E1" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      E1
+    </div>
+    <div id="F1" class="square even" @drop.prevent="drop" @dragover.prevent>
+      F1
+    </div>
+    <div id="G1" class="square odd" @drop.prevent="drop" @dragover.prevent>
+      G1
+    </div>
+    <div id="H1" class="square even" @drop.prevent="drop" @dragover.prevent>
+      H1
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      difficulty: "random",
-      char: "",
-      result: "",
-      squares: [
-        {
-          id: 0,
-          value: "",
-        },
-        {
-          id: 1,
-          value: "",
-        },
-        {
-          id: 2,
-          value: "",
-        },
-        {
-          id: 3,
-          value: "",
-        },
-        {
-          id: 4,
-          value: "",
-        },
-        {
-          id: 5,
-          value: "",
-        },
-        {
-          id: 6,
-          value: "",
-        },
-        {
-          id: 7,
-          value: "",
-        },
-        {
-          id: 8,
-          value: "",
-        },
-      ],
-      winningSquares: [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ],
-    };
+    return {};
   },
   methods: {
-    setDifficulty(event) {
-      this.difficulty = event.target.value;
+    // allowDrop(event) {
+    //   console.log(event);
+    // },
+    drag(event) {
+      event.dataTransfer.setData("text", event.target.id);
     },
-    chooseRandomSquare() {
-      let selectedSquare;
-      do {
-        const num = Math.floor(Math.random() * 9);
-        selectedSquare = this.squares[num];
-      } while (selectedSquare.value !== "");
-      return selectedSquare;
-    },
-
-    chooseBlockingSquare(char) {
-      for (let winningSquaresTriple of this.winningSquares) {
-        const numXs = winningSquaresTriple.filter(
-          (squareId) => this.squares[squareId].value === char
-        ).length;
-        if (numXs != 2) {
-          continue;
-        }
-        const emptySquareId = winningSquaresTriple.find(
-          (squareId) => this.squares[squareId].value === ""
-        );
-        if (emptySquareId === undefined) {
-          continue;
-        }
-        // Go one emptySquare
-        return this.squares[emptySquareId];
-      }
-      return null;
-    },
-
-    chooseMiddleSquare() {
-      return this.squares[4].value === "" ? this.squares[4] : null;
-    },
-
-    computeNext() {
-      if (
-        this.difficulty === "random" ||
-        (this.difficulty === "medium" && Math.random() < 0.2)
-      ) {
-        return this.chooseRandomSquare();
-      }
-      let selectedSquare = this.chooseMiddleSquare();
-      if (selectedSquare != null) {
-        return selectedSquare;
-      }
-      selectedSquare = this.chooseBlockingSquare("O");
-      if (selectedSquare != null) {
-        return selectedSquare;
-      }
-      selectedSquare = this.chooseBlockingSquare("X");
-      if (selectedSquare != null) {
-        return selectedSquare;
-      }
-      return this.chooseRandomSquare();
-    },
-
-    computerInput() {
-      if (this.result !== "") {
-        return;
-      }
-      this.char = "O";
-      const selectedSquare = this.computeNext();
-      selectedSquare.value = this.char;
-      if (this.check()) {
-        this.result = "computer";
-      }
-    },
-
-    playerInput(id) {
-      if (this.result !== "") {
-        return;
-      }
-      this.char = "X";
-      const selectedSquare = this.squares[id];
-      if (selectedSquare.value === "") {
-        selectedSquare.value = this.char;
-        if (this.check()) {
-          this.result = "player";
-        } else if (this.squares.find((square) => square.value === "")) {
-          this.computerInput();
-        } else {
-          this.result = "draw";
-        }
-      }
-    },
-    check() {
-      for (let i = 0; i < this.winningSquares.length; i++) {
-        let [a, b, c] = this.winningSquares[i];
-        let squareA = this.squares[a];
-        let squareB = this.squares[b];
-        let squareC = this.squares[c];
-        if (
-          squareA.value === this.char &&
-          squareB.value === this.char &&
-          squareC.value === this.char
-        ) {
-          return true;
-        }
-      }
-      return false;
-    },
-    reset() {
-      this.char = "";
-      this.result = "";
-      this.squares = [
-        {
-          id: 0,
-          value: "",
-        },
-        {
-          id: 1,
-          value: "",
-        },
-        {
-          id: 2,
-          value: "",
-        },
-        {
-          id: 3,
-          value: "",
-        },
-        {
-          id: 4,
-          value: "",
-        },
-        {
-          id: 5,
-          value: "",
-        },
-        {
-          id: 6,
-          value: "",
-        },
-        {
-          id: 7,
-          value: "",
-        },
-        {
-          id: 8,
-          value: "",
-        },
-      ];
+    drop(event) {
+      let data = event.dataTransfer.getData("text");
+      event.target.appendChild(document.getElementById(data));
     },
   },
 };
@@ -227,24 +288,34 @@ export default {
 
 <style>
 #grid {
-  display: grid;
-  grid-template-columns: repeat(3, minmax(0, 1fr));
+  display: inline-grid;
+  grid-template-columns: repeat(8, minmax(0, 1fr));
+  column-gap: 0;
 }
-
 .square {
   border-style: solid;
   border-width: 2px;
-  padding: 16px;
-  /* font-size: 100px; */
   text-align: center;
+  height: 50px;
+  width: 50px;
+}
+@media (width >= 600px) and (height >= 600px) {
+  .square {
+    height: 75px;
+    width: 75px;
+  }
+}
+@media (width >= 800px) and (height >= 800px) {
+  .square {
+    height: 100px;
+    width: 100px;
+  }
 }
 
-/* .square-wrapper {
-  display: inline-block;
-  border-style: solid;
-  border-width: 2px;
-  height: 0;
-  width: 33%;
-  padding-bottom: 33%;
-} */
+.odd {
+  background-color: white;
+}
+.even {
+  background-color: grey;
+}
 </style>
